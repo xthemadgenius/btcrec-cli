3819d20e1f5780ae6ba873c80ba1b3f75eb53bd5
3rdIteration <steve@cryptoguide.tips>
2019-11-21 18:04:26 +1000

    Update .travis.install-dependencies.sh

--- .travis.install-dependencies.sh
+++ .travis.install-dependencies.sh
@@ -8,13 +8,13 @@
 set -e
 
 # Download and install Armory v0.96.0 plus prerequisites
-LATEST="https://github.com/goatpig/BitcoinArmory/releases/download/v0.96/armory_0.96-gcc4.7_amd64.deb"
+LATEST="https://github.com/goatpig/BitcoinArmory/releases/download/v0.96/armory_0.96-gcc5.3_amd64.deb"
 
 curl -LfsS --retry 10 -o 'armory.deb' "$LATEST"
 
 sudo apt-get -q update
 sudo apt-get -yq install gdebi-core
-sudo gdebi -nq armory.deb Don't install armory until fixed crash
+sudo gdebi -nq armory.deb
 
 # Download, compile, and install prerequisites for bitcoinj wallets
 
