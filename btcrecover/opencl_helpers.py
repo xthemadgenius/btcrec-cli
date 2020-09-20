708f9e4af613bceff1e9b1696af290bac1980619
Gaith Hallak <gaithhallak@gmail.com>
2020-09-20 13:52:23 +0300

    add bip38 wallet to opencl_helpers

--- btcrecover/opencl_helpers.py
+++ btcrecover/opencl_helpers.py
@@ -69,6 +69,8 @@ def init_opencl_contexts(loaded_wallet, openclDevice = 0):
     elif type(loaded_wallet) is btcrecover.btcrpass.WalletBitcoinCore:
         loaded_wallet.opencl_context_hash_iterations_sha512 = loaded_wallet.opencl_algo.cl_hash_iterations_init(
             "sha512")
+    elif type(loaded_wallet) is btcrecover.btcrpass.WalletBIP38:
+        loaded_wallet.opencl_context_scrypt = loaded_wallet.opencl_algo.cl_scrypt_init(14)
     elif type(loaded_wallet) in (btcrecover.btcrpass.WalletBIP39, btcrecover.btcrpass.WalletElectrum28):
         salt = b"mnemonic"
         loaded_wallet.opencl_context_pbkdf2_sha512 = loaded_wallet.opencl_algo.cl_pbkdf2_init("sha512",
