85e6fdcf6f4fcfc37f80ebed0201aa37bdd8b553
Jefferson Nunn <msappdev2008@gmail.com>
2020-12-25 10:46:26 -0600

    Update btcrecover.py

--- btcrecover.py
+++ btcrecover.py
@@ -16,13 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/
 
-# If you find this program helpful, please consider a small
-# donation to the developer at the following Bitcoin address:
-#
-#           3Au8ZodNHPei7MQiSVAWb7NB2yqsb48GW4
-#
-#                      Thank You!
-
 # PYTHON_ARGCOMPLETE_OK - enables optional bash tab completion
 
 import compatibility_check
